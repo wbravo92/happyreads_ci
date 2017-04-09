54cb6d68b5b63f0df37ba72ff9c095aa68a5f80c
Winston Bravo <wbravoanoni@gmail.com>
2017-04-09 18:35:30 -0300

    Archivo base codeigniter

---
+++ application/views/home.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="es">
+<meta charset="utf-8">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+</head>
+<body>
+
+<div id="container">
+	<h1>Bienvenido a Codeigniter</h1>
+
+	<div id="body">
+		<p>Este es el cuerpo del Codigo Codeigniter</p>
+
+
+	</div>
+
+	
+</div>
+
+</body>
+</html>
---
+++ application/views/panel.php
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+
+<h1>Este es el Menu del panel</h1>
+
+
+<?php echo $this->calendar->generate()?>
+
+</body>
+</html>		
